02c1f597648fcbfcf78233e2ee0cd2dbe3910c63
Khaled Abdulhamid <khaledraedabdulhamed101@hotmail.com>
2024-01-06 20:51:58 -0500

    Adding a fix and many features

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { currentUser } from '@clerk/nextjs'
-import { redirect } from 'next/navigation'
-import { fetchUser } from '@/lib/actions/user.actions'
-import ProfileHeader from '@/components/shared/ProfileHeader'
-import { Tabs, TabsList, TabsContent, TabsTrigger } from '@/components/ui/tabs'
-import { profileTabs } from '@/constants'
-import Image from 'next/image'
-import ThreadsTabs from '@/components/shared/ThreadsTabs'
+import Image from "next/image";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { profileTabs } from "@/constants";
+
+import ThreadsTabs from "@/components/shared/ThreadsTabs";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { fetchUser } from "@/lib/actions/user.actions";
 
 async function Page({ params }: { params: { id: string } }) {
   const user = await currentUser();
@@ -17,27 +19,37 @@ async function Page({ params }: { params: { id: string } }) {
 
   return (
     <section>
-    <ProfileHeader
-      accountId={userInfo.id}
-      authUserId={user.id}
-      name={userInfo.name}
-      username={userInfo.username}
-      imageUrl={userInfo.image}
-      bio={userInfo.bio}
-    />
-        <div className='mt-9'>
-      <Tabs defaultValue='threads' className='w-full'>
-        <TabsList className='tab'>
-        {profileTabs.map((tab)=> (
-          <TabsTrigger key={tab.label} value={tab.value} className='tab'> 
-            <Image src={tab.icon} alt={tab.label} width={24} height={24} className='object-contain'/>
-            <p className='max-sm:hidden'>{tab.label}</p>
-            {tab.label === 'Threads' && (<p className='ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2'>
-              {userInfo?.threads?.length}
-            </p>)}
-          </TabsTrigger>
-        ))}
-         </TabsList>
+      <ProfileHeader
+        accountId={userInfo.id}
+        authUserId={user.id}
+        name={userInfo.name}
+        username={userInfo.username}
+        imageUrl={userInfo.image}
+        bio={userInfo.bio}
+      />
+
+      <div className='mt-9'>
+        <Tabs defaultValue='threads' className='w-full'>
+          <TabsList className='tab'>
+            {profileTabs.map((tab) => (
+              <TabsTrigger key={tab.label} value={tab.value} className='tab'>
+                <Image
+                  src={tab.icon}
+                  alt={tab.label}
+                  width={24}
+                  height={24}
+                  className='object-contain'
+                />
+                <p className='max-sm:hidden'>{tab.label}</p>
+
+                {tab.label === "Threads" && (
+                  <p className='ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2'>
+                    {userInfo.threads.length}
+                  </p>
+                )}
+              </TabsTrigger>
+            ))}
+          </TabsList>
           {profileTabs.map((tab) => (
             <TabsContent
               key={`content-${tab.label}`}
